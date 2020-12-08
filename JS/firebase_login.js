b0b1fcb2db000a2be736b3c8d0258bc8945ffae3
RapidAkhil10 <54390056+RapidAkhil10@users.noreply.github.com>
2020-12-08 22:47:15 +0530

    Update firebase_login.js

--- JS/firebase_login.js
+++ JS/firebase_login.js
@@ -20,8 +20,9 @@
 
   //var docRef = firestore.collection("Users").doc(email.value);
 
-  login.addEventListener("click", e =>)
+  login.addEventListener("click", e => {
     e.preventDefault();
+
 
     const promise = auth.signInWithEmailAndPassword(email.value, pass.value)
     .then(() => {
